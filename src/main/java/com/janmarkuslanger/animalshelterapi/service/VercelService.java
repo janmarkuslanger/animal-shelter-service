c0b63e24815e2ef8c8783701a3fe58a6d2e7f86f
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2024-09-13 21:25:21 +0200

    Add vercel service

---
+++ src/main/java/com/janmarkuslanger/animalshelterapi/service/VercelService.java
@@ -0,0 +1,22 @@
+package com.janmarkuslanger.animalshelterapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class VercelService {
+
+    private final String hookUrl;
+
+    @Autowired
+    public VercelService(@Value("vercel.url") String hookUrl) {
+        this.hookUrl = hookUrl;
+    }
+
+    public void triggerDeployment() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForEntity(hookUrl, null, String.class);
+    }
+}
