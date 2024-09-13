38390f06780093456a1e3e0bb4a16f89751302ee
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2024-09-13 21:19:14 +0200

    Implemenr user table

--- src/main/java/com/janmarkuslanger/animalshelterapi/model/User.java
+++ src/main/java/com/janmarkuslanger/animalshelterapi/model/User.java
@@ -1,11 +1,9 @@
 package com.janmarkuslanger.animalshelterapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
